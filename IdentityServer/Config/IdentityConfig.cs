4053e9b2a083a6a938da37e04f58f9ec3ea81fa1
Dmitry Karyakin <375447955552dk@gmail.com>
2021-11-17 23:45:23 +0300

    add auth prolects

---
+++ IdentityServer/Config/IdentityConfig.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace IdentityServer.Config
+{
+    public static class IdentityConfig
+    {
+        public static IEnumerable<IdentityResource> IdentityResources
+        {
+            get
+            {
+                yield return new IdentityResources.OpenId();
+                yield return new IdentityResources.Profile();
+            }
+        }
+
+        public static IEnumerable<ApiScope> ApiScopes
+        {
+            get
+            {
+                yield return new ApiScope("api1", "My API");
+            }
+        }
+
+        public static IEnumerable<Client> Clients
+        {
+            get
+            {
+                // machine to machine client
+                yield return new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                };
+
+                // interactive ASP.NET Core MVC client
+                yield return new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,78 @@
+﻿//using IdentityServer.Config;
+//using IdentityServer.Quickstart;
+
+using IdentityServer.Config;
+using IdentityServer.Quickstart;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }// позволяет получить доспуп к окружению https://metanit.com/sharp/aspnet5/2.21.php
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddRazorPages();
+            services.AddControllersWithViews();
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(IdentityConfig.IdentityResources)
+                .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
+                .AddInMemoryClients(IdentityConfig.Clients)
+                .AddTestUsers(TestUsers.Users);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+            services.Google(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            /* app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapRazorPages();
+             });*/
+        }
+    }
+}
+/*Не забыть создать серкеты в гугл-консоли для использования гугловой авторизации. Есть туториалы*/
